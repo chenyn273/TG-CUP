19cff02a7d500dbcf28242d91539f48a36681eb2
chenyn273 <121368524+chenyn273@users.noreply.github.com>
2024-03-03 10:31:32 +0800

    Update tokenize.py

--- TG_GGNN/tokenizer/tokenize.py
+++ TG_GGNN/tokenizer/tokenize.py
@@ -24,20 +24,6 @@ def train(input_file, vocab_size, model_name, model_type, character_coverage):
 
 
 def run():
-    # en_input = '../data/corpus.en'
-    # en_vocab_size = 32000
-    # en_model_name = 'eng'
-    # en_model_type = 'bpe'
-    # en_character_coverage = 1
-    # train(en_input, en_vocab_size, en_model_name, en_model_type, en_character_coverage)
-    #
-    # ch_input = '../data/corpus.ch'
-    # ch_vocab_size = 32000
-    # ch_model_name = 'chn'
-    # ch_model_type = 'bpe'
-    # ch_character_coverage = 0.9995
-    # train(ch_input, ch_vocab_size, ch_model_name, ch_model_type, ch_character_coverage)
-
     code_input = '../data/corpus.code'
     code_vocab_size = 32000
     code_model_name = 'code'
@@ -47,14 +33,6 @@ def run():
 
 
 def test():
-    # sp = spm.SentencePieceProcessor()
-    # text = "美国总统特朗普今日抵达夏威夷。"
-    #
-    # sp.Load("./chn.model")
-    # print(sp.EncodeAsPieces(text))
-    # print(sp.EncodeAsIds(text))
-    # a = [12907, 277, 7419, 7318, 18384, 28724]
-    # print(sp.decode_ids(a))
     sp = spm.SentencePieceProcessor()
     text = "Save cluster basic configuration."
     sp.Load("./code.model")
